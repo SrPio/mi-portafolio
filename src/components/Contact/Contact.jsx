4ec318c05381f6298b48ff6a963aca665b32b19c
SrPio <andresfer1799@gmail.com>
2023-04-15 17:03:22 -0500

    feat(Contact): :zap: Implementación de envío de formulario

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,96 @@
 import React from "react";
 import styles from "./Contact.module.css";
 import { motion } from "framer-motion";
+import { useForm, ValidationError } from "@formspree/react";
 
 function Contact({ isDarkMode }) {
+  const [state, handleSubmit] = useForm("mgebpbwa");
+  if (state.succeeded) {
+    return (
+      <div
+        id="contact"
+        className={`${isDarkMode ? styles.dark : ""} ${
+          styles.container_contact
+        }`}
+      >
+        <div className={styles.contact}>
+          <div
+            className={`${isDarkMode ? styles.dark : ""} ${
+              styles.container_info_contact
+            }`}
+          >
+            <form
+              className={`${isDarkMode ? styles.dark : ""} ${
+                styles.contact_form
+              }`}
+            >
+              <h2 className={styles.form__submitted__text}>
+                ¡Gracias por contactarme!
+              </h2>
+            </form>
+            <div className={styles.contanier_mobile_btns}>
+              <button
+                className={styles.btn_linkedin}
+                onClick={() =>
+                  window.open(
+                    "https://www.linkedin.com/in/andres-fernando-jaramillo-avila/",
+                    "_blank"
+                  )
+                }
+              >
+                <img
+                  loading="lazy"
+                  src="/assets/linkedin-icon.svg"
+                  alt="icon"
+                />
+                Contáctame por Linkedin
+              </button>
+              <button
+                className={styles.btn_github}
+                onClick={() =>
+                  window.open("https://github.com/SrPio", "_blank")
+                }
+              >
+                <img loading="lazy" src="/assets/github-icon.svg" alt="icon" />
+                Contáctame por GitHub
+              </button>
+            </div>
+            <div className={styles.container_networks}>
+              <motion.img
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                loading="lazy"
+                src="/assets/linkedin-icon.svg"
+                alt="icon"
+                className={styles.icon_linkedin}
+                onClick={() =>
+                  window.open(
+                    "https://www.linkedin.com/in/andres-fernando-jaramillo-avila/",
+                    "_blank"
+                  )
+                }
+              />
+              <motion.img
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                loading="lazy"
+                src="/assets/github-icon.svg"
+                alt="icon"
+                className={styles.icon_github}
+                onClick={() =>
+                  window.open("https://github.com/SrPio", "_blank")
+                }
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          className={`${isDarkMode ? styles.dark : ""} ${styles.footer}`}
+        ></div>
+      </div>
+    );
+  }
+
   return (
     <div
       id="contact"
@@ -18,41 +106,79 @@ function Contact({ isDarkMode }) {
             className={`${isDarkMode ? styles.dark : ""} ${
               styles.contact_form
             }`}
-            action=""
+            onSubmit={handleSubmit}
           >
             <h1>¡Trabajemos juntos!</h1>
             <div className={styles.form_gruop}>
-              <label htmlFor="">Nombre</label>
+              <label htmlFor="nombre">Nombre</label>
               <input
                 className={styles.input_contact}
                 type="text"
+                id="nombre"
+                name="nombre"
                 placeholder="Escribe tu nombre"
               />
-            </div>
-            <div className={styles.form_gruop}>
-              <label htmlFor="">Email</label>
-              <input
-                className={styles.input_contact}
-                type="email"
-                placeholder="Escribe tu email"
+              <ValidationError
+                prefix="Nombre"
+                field="nombre"
+                errors={state.errors}
               />
             </div>
             <div className={styles.form_gruop}>
-              <label htmlFor="">Mensaje</label>
+              <label htmlFor="email">Email</label>
+              <input
+                className={styles.input_contact}
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Escribe tu email"
+              />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
+            </div>
+            <div className={styles.form_gruop}>
+              <label htmlFor="mensaje">Mensaje</label>
               <textarea
                 className={styles.input_area}
                 type="text"
+                id="mensaje"
+                name="mensaje"
                 placeholder="Escribe tu mensaje"
               />
+              <ValidationError
+                prefix="Mensaje"
+                field="mensaje"
+                errors={state.errors}
+              />
             </div>
-            <button className={styles.btn_send_contact}>Enviar</button>
+            <button
+              className={styles.btn_send_contact}
+              type="submit"
+              disabled={state.submitting}
+            >
+              Enviar
+            </button>
           </form>
           <div className={styles.contanier_mobile_btns}>
-            <button className={styles.btn_linkedin}>
+            <button
+              className={styles.btn_linkedin}
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/andres-fernando-jaramillo-avila/",
+                  "_blank"
+                )
+              }
+            >
               <img loading="lazy" src="/assets/linkedin-icon.svg" alt="icon" />
               Contáctame por Linkedin
             </button>
-            <button className={styles.btn_github}>
+            <button
+              className={styles.btn_github}
+              onClick={() => window.open("https://github.com/SrPio", "_blank")}
+            >
               <img loading="lazy" src="/assets/github-icon.svg" alt="icon" />
               Contáctame por GitHub
             </button>
@@ -65,6 +191,12 @@ function Contact({ isDarkMode }) {
               src="/assets/linkedin-icon.svg"
               alt="icon"
               className={styles.icon_linkedin}
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/andres-fernando-jaramillo-avila/",
+                  "_blank"
+                )
+              }
             />
             <motion.img
               whileHover={{ scale: 1.1 }}
@@ -73,6 +205,7 @@ function Contact({ isDarkMode }) {
               src="/assets/github-icon.svg"
               alt="icon"
               className={styles.icon_github}
+              onClick={() => window.open("https://github.com/SrPio", "_blank")}
             />
           </div>
         </div>
